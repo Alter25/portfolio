fcb1260e7e1cd6360ede1957d41a8fd44756ed6a
jorge <jf.lozano.n@gmail.com>
2026-04-01 14:30:39 -0600

    page:Login

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css'
 import Home from './pages/Home'
 import Admin from "./pages/Admin";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/admin" element={
           <ProtectedRoute>
             <Admin />
