1ec7394c7287cec47ae51fb9065467b2fa950c1d
jorge <jf.lozano.n@gmail.com>
2026-04-01 17:22:28 -0600

    feat(ui): add close button for modal & website link

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -1,4 +1,5 @@
 import type { Project } from "../types/project";
+import { IoClose } from "react-icons/io5";
 
 interface Props {
   project: Project | null;
@@ -29,8 +30,8 @@ export default function ProjectModal({ project, onClose }: Props) {
             </h2>
             <button
               onClick={onClose}
-              className="text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors text-lg leading-none"
-            ></button>
+              className="text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors text-lg p-1 hover:bg-black/10 hover:cursor-pointer border-2 rounded-full leading-none"
+            ><IoClose /></button>
           </div>
           {
             project.description && (
@@ -43,23 +44,37 @@ export default function ProjectModal({ project, onClose }: Props) {
                 <p className="text-xs text-zinc-400 mb-2">Stack</p>
                 <div className="flex flex-wrap gap-1">
                   {project.tags.map(tag => (
-                    <span key={tag} className="text-xs px-2 py-0.5 rounded-full bg-zinc-100 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400">{tag}</span>
+                    <div key={tag} className="text-xs px-2 py-1 rounded-full bg-zinc-400 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400 border border-zinc-800 dark:border-zinc-200">{tag}</div>
                   ))}
                 </div>
               </div>
             )
           }
-          <div className="flex gap-2">
-            {
-              project.live_url && (
-                <a
-                  href={project.live_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-sm px-4 py-2 rounded-lg border border-zinc-200 dark:border-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-50 dark:hover:bg-zinc-800 transition-colors"
-                >GitHub</a>
-              )
-            }
+          <div className="w-full flex gap-3">
+            <div className="flex gap-2">
+              {
+                project.repo_url && (
+                  <a
+                    href={project.repo_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm px-4 py-1 rounded-lg border border-zinc-200 dark:border-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-50 dark:hover:bg-zinc-800 transition-colors"
+                  >GitHub</a>
+                )
+              }
+            </div>
+            <div className="flex gap-2">
+              {
+                project.live_url && (
+                  <a
+                    href={project.live_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm px-4 py-1 rounded-lg border border-zinc-200 dark:border-zinc-700 text-zinc-700 dark:text-zinc-300 hover:bg-zinc-50 dark:hover:bg-zinc-800 transition-colors"
+                  >Sitio web</a>
+                )
+              }
+            </div>
           </div>
         </div>
       </div>
